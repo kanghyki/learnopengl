3c60c44843c96c24b2b5a02e2f4933afa774aaa0
kanghyki <kanghyki@gmail.com>
2024-03-21 10:29:37 +0900

    rectangle ebo

--- src/main.cpp
+++ src/main.cpp
@@ -67,19 +67,30 @@ int main(int argc, char** argv)
     fragmentShader = nullptr;
 
     float vertices[] = {
+         0.5f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    }; 
+        -0.5f,  0.5f, 0.0f
+    };
 
-    // VAO/VBO
-    unsigned int VAO, VBO;
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    // VAO/VBO/EBO
+    unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);  
     glGenBuffers(1, &VBO);  
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);  
@@ -88,6 +99,8 @@ int main(int argc, char** argv)
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
     glBindVertexArray(0); 
 
+    // wireframe
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     SPDLOG_INFO("Start render loop");
     while (!glfwWindowShouldClose(window))
@@ -100,7 +113,8 @@ int main(int argc, char** argv)
         // Draw
         glUseProgram(program->get());
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -108,6 +122,7 @@ int main(int argc, char** argv)
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     program = nullptr;
 
     glfwTerminate();
