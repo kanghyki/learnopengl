2713f29b2d9b8a24a976f7a5b923a488b6b91f23
kanghyki <kanghyki@gmail.com>
2024-03-25 11:52:52 +0900

    shader uniform

--- src/Context.cpp
+++ src/Context.cpp
@@ -37,6 +37,11 @@ void Context::render()
     }
     ImGui::End();
 
+    float timeValue = glfwGetTime();
+    float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+    int vertexColorLocation = glGetUniformLocation(mProgram->get(), "ourColor");
+    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
     glClear(GL_COLOR_BUFFER_BIT);
     mProgram->Use();
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
