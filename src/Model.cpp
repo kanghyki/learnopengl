0e63674f69fd7881b5c6339bd948c7bb966b58e3
kanghyki <kanghyki@gmail.com>
2024-04-03 18:52:00 +0900

    add normal

--- src/Model.cpp
+++ src/Model.cpp
@@ -68,36 +68,48 @@ bool Model::parseObjFormat(const std::string& data)
         else if (prefix == "f")
         {
             std::vector<VertexIndex> vi;
-
             for (size_t i = 0; i < word.size(); ++i)
             {
                 std::vector<std::string> vtx = split(word[i], "/");
-                int vIndex = std::stoi(vtx[0]) - 1;
-                int vtIndex = -1;
-                int vnIndex = -1;
+                VertexIndex index = { -1, -1 , -1 };
+                index.v += std::stoi(vtx[0]);
                 if (vtx.size() == 3) {
-                    vtIndex += std::stoi(vtx[1]);
-                    vnIndex += std::stoi(vtx[2]);
+                    index.vt += std::stoi(vtx[1]);
+                    index.vn += std::stoi(vtx[2]);
                 }
-                vi.push_back({vIndex, vtIndex, vnIndex});
+                vi.push_back(index);
             }
 
-            size_t beforeVertexSize = mVertexes.size();
+            std::vector<Vertex> tempV;
             for (size_t i = 0; i < word.size(); ++i)
             {
                 VertexIndex index = vi[i];
                 glm::vec3 v = tempVertexes[index.v];
                 glm::vec3 vn = index.vn != -1 ? tempNormals[index.vn] : glm::vec3(0.0f);
                 glm::vec2 vt = index.vt != -1 ? tempTexCoords[index.vt] : glm::vec2(0.0f);
-                mVertexes.push_back({v, vn, vt});
+                tempV.push_back({v, vn, vt});
             }
 
+            glm::vec3 v0 = tempV[1].position - tempV[0].position;
+            glm::vec3 v1 = tempV[2].position - tempV[0].position;
+            glm::vec3 n = glm::cross(v0, v1);
+            for (size_t i = 0; i < vi.size(); ++i)
+            {
+                if (vi[i].vn == -1)
+                {
+                    tempV[i].normal = n;
+                }
+            }
+
+            size_t beforeVertexSize = mVertexes.size();
             for (size_t i = 0; i < word.size() - 2; ++i)
             {
                 mIndices.push_back(beforeVertexSize);
                 mIndices.push_back(beforeVertexSize + i + 1);
                 mIndices.push_back(beforeVertexSize + i + 2);
             }
+
+            mVertexes.insert(mVertexes.end(), tempV.begin(), tempV.end());
         }
     }
 
